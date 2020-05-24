ca094e4675a8ddd3f06696b7a20052cc3a7722e9
marciberes <55618666+marciberes@users.noreply.github.com>
2020-05-24 13:02:13 +0200

    Create pattog.c

---
+++ thematic_tutorials/bhax_konyvem/Turing/pattog.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <curses.h>
+#include <unistd.h>
+
+int main (void){
+
+	WINDOW *ablak;
+	ablak = initscr ();
+	
+	int x=0;
+	int y=0;
+
+	int deltax=1;
+	int deltay=1;
+
+	int mx;
+	int my;
+
+for (;;){
+
+	getmaxyx(ablak , mx , my);
+
+	mvprintw(y , x , 'o');
+
+	refresh();
+	usleep(100000);
+
+	clear();
+
+	x=x+deltax;
+	y=y+deltay;
+
+
+	if( x <=0 ){
+		deltax=deltax * -1;
+	}
+	if( x >=mx-1 ){
+		deltax= deltax * -1;
+	}
+	if( y<=3){
+		deltay=deltay * -1;
+	}
+	if( y>=my-1){
+		deltay=deltay * -1;
+	}
+}
+return 0;
+}
