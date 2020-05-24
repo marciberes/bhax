61f6471b2c1cf3f92e5a1eb6ede2211c12c0532c
marciberes <55618666+marciberes@users.noreply.github.com>
2020-05-24 22:51:20 +0200

    Add files via upload

---
+++ thematic_tutorials/bhax_konyvem/Chomsky/decimalis.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+int a = 0;
+	printf("Irj be egy számot: ");
+	scanf("%d",&a);
+	printf("Unáris megjelenése: ");
+	for(int i = 0; i<a;++i){
+		printf("|");
+}
+printf("\n");
+return 0;
+}
+
